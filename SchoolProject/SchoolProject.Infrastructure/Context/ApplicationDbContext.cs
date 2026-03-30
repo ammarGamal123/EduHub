3b00c29654f6ef5f8f3dc68ad2f80fe24361df6c
Ammar Hammad <amargamal832909@gmail.com>
2026-03-30 17:28:02 +0200

    chore: move git repo root to CleanArchitecture folder

---
+++ SchoolProject/SchoolProject.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+       
+        public DbSet<Student> Students { get; set; }    
+        public DbSet<Department> Departments { get;set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+        public DbSet<DepartmentSubject> DepartmentSubjects { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<InstructorSubject> InstructorSubjects { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<InstructorSubject>()
+                .HasKey(i => new { i.SubjectID, i.InstructorID });
+
+            modelBuilder.Entity<StudentSubject>()
+                .HasKey(s => new { s.SubID, s.StudID });
+
+            modelBuilder.Entity<DepartmentSubject>()
+                .HasKey(d => new { d.SubID, d.DeptID });
+
+            modelBuilder.Entity<Instructor>()
+                .HasOne(i => i.Supervisor)
+                .WithMany(i => i.Instructors)
+                .HasForeignKey(i => i.SupervisorID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Department>()
+                .HasOne(d => d.Instructor)
+                .WithOne(d => d.DepartmentManager)
+                .HasForeignKey<Department>(d => d.InstructorManager)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
